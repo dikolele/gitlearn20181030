d75a573c20cf58b9a0370b8c9ba4d89f7e86846b
zhangqianle <zhangqianle@192.168.0.100>
2018-10-30 23:21:30 +0800

    git第一次提交

---
+++ src/main/java/com/zql/org/FirstClass.java
@@ -0,0 +1,18 @@
+/**
+ * <B>仉乾乐</B><BR>
+ * <B>系统名称：自建项目</B><BR>
+ * <B>模块名称：git-learn</B><BR>
+ * <B>中文类名：</B><BR>
+ * <B>概要说明：</B><BR>
+ * @author zhangql
+ * @since 2018年10月30日
+ */
+package com.zql.org;
+
+public class FirstClass {
+
+	public static void main(String[] args) {
+		System.out.println("这里是git练习！");
+	}
+
+}
